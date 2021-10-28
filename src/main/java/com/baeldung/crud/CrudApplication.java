0f9e2c7727f2a2412adc307fa07c2908e72743a7
juldut <julius.prayogo@gmail.com>
2021-10-28 11:03:56 +0700

    add twitter bootstrap 5.0 only apply in add-user static resource example navbar-demo.html

--- src/main/java/com/baeldung/crud/CrudApplication.java
+++ src/main/java/com/baeldung/crud/CrudApplication.java
@@ -2,6 +2,9 @@ package com.baeldung.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -9,5 +12,10 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
+
+	@Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
 }
--- src/main/java/com/baeldung/crud/repository/UserRepository.java
+++ src/main/java/com/baeldung/crud/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.baeldung.crud.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.baeldung.crud.domain.User;
 
 
-public interface UserRepository extends CrudRepository<User, Long> {
+//public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
